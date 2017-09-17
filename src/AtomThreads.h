091fcaaaaa9702bf53fcac9534e60a7e0d48abd2
Lupino <lmjubuntu@gmail.com>
2017-09-17 12:37:50 +0800

    Initial Commit

---
+++ src/AtomThreads.h
@@ -0,0 +1,12 @@
+#ifndef ATOMTHREAD_AVR_h
+#define ATOMTHREAD_AVR_h
+
+#include "kernel/atom.h"
+#include "atomport-private.h"
+#include "atomport.h"
+#include "kernel/atomtimer.h"
+#include "kernel/atomqueue.h"
+#include "kernel/atommutex.h"
+#include "kernel/atomsem.h"
+
+#endif  // ATOMTHREAD_AVR_h
